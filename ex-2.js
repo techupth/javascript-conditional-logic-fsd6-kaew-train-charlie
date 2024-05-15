fe9bc9ba0a333183b6860dc89eb3331026da6cca
trainthunder <kitikun.po@gmail.com>
2024-05-15 13:09:11 +0700

    feat: TraiHackhour

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,11 @@
 //Exercise 2
-let lightBulbStatus = "On";
+let lightBulbStatus = "Broken";
 
 // Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+} else {
+  console.log("Please choose the correct input (On/Off/Broken)");
+}
